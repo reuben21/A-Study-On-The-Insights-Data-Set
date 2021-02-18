95ce9cf5a10d1f3efdab10b264201bdcfe164927
Reuben Coutinho <reuben211999@icloud.com>
2021-02-18 22:42:05 +0530

    Updated Session Locally

--- react-front-end/src/Components/Sections/Section1/Question/question2.js
+++ react-front-end/src/Components/Sections/Section1/Question/question2.js
@@ -1,20 +1,20 @@
 import React, {Component} from 'react';
 import {Button} from "@material-ui/core";
 import {NavLink} from "react-router-dom";
-import { Scatter} from "react-chartjs-2";
-import 'chart.piecelabel.js';
 import * as Colors from "../../../../COLORS";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 class QUESTION extends Component {
     state = {
         key: [],
         value: [],
-        colors: []
+        colors: [],
+        loading: true,
     }
 
-    componentDidMount() {
+    async componentDidMount() {
 
-        fetch('http://127.0.0.1:8000/section1/question1', {
+       const state = await fetch('http://127.0.0.1:8000/section1/question1', {
             method: 'GET',
             // headers: {
             //     'Content-Type': 'application/json'
@@ -25,57 +25,46 @@ class QUESTION extends Component {
             .then(data => {
                 console.log(data)
                 this.setState({
-                    key: data.labels,
-                    value: data.values,
-                    colors: data.colors
+                    // key: data.labels,
+                    value: data.ImageBytes,
+
+                    // colors: data.colors,
 
                 })
                 return data;
             }).catch((error) => {
             return error;
         })
+        if(state){
+             this.setState({
+            loading: false
+        })
+        }
+
     }
 
 
     render() {
-        const data = {
-            datasets: [{
-                label: 'Scatter Dataset',
-                data: [{
-                    x: -10,
-                    y: 0
-                }, {
-                    x: 0,
-                    y: 10
-                }, {
-                    x: 10,
-                    y: 5
-                }]
-            }]
-        };
-
-        const options = {
-            scales: {
-                xAxes: [{
-                    type: 'linear',
-                    position: 'bottom'
-                }]
-            }
+        if (this.state.loading) {
+            return <CircularProgress style={{
+                marginTop:"100px",
+                color: Colors.DARK_GREENISH,
+            }}/>;
         }
+        // isVerifyLoading ?  : <></>
 
 
         return (
-
             <>
 
                 <div>
                     <h1 style={{
                         fontFamily: "'Montserrat', sans-serif",
                         color: Colors.DARK_GREENISH
-                    }
-                    }>Predict the most profitable product that the company must keep manufacturing</h1>
+                    }}>Predict the most profitable product that the company must keep manufacturing</h1>
                 </div>
-                <Scatter data={data} options={options}/>
+                {/*<Bar data={data} options={options}/>*/}
+                <img src={"data:image/png;base64," + this.state.value} alt=""/>
                 <div>
                     <Button component={NavLink} to={"/home"} style={{
                         fontFamily: "'Montserrat', sans-serif",
@@ -85,8 +74,7 @@ class QUESTION extends Component {
                     }}>Back</Button>
                 </div>
             </>
-        )
-            ;
+        );
     }
 
 
