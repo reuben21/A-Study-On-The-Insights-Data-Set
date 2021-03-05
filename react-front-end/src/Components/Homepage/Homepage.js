5f0aaa11e0db8274a1aff4d30e8225df34e7bb9b
reuben21 <reuben211999@icloud.com>
2021-03-06 00:08:28 +0530

    Updated with chatbot

--- react-front-end/src/Components/Homepage/Homepage.js
+++ react-front-end/src/Components/Homepage/Homepage.js
@@ -41,216 +41,104 @@ const Homepage = () => {
     console.log("The Session is", statusOfHuman)
     const url = "http://localhost:8000/success";
 
-    const runFacemesh = async () => {
-        // OLD MODEL
-        // const net = await facemesh.load({
-        //   inputResolution: { width: 640, height: 480 },
-        //   scale: 0.8,
-        // });
-        // NEW MODEL
-        const model = await blazeface.load();
-        const returnTensors = false;
-        // const net = await facemesh.load(facemesh.SupportedPackages.mediapipeFacemesh);
-        setInterval(() => {
-            return detect(model, returnTensors, 640, 480);
-        }, 10);
-    };
 
-    const detect = async (model, returnTensors, width, height) => {
-        if (
-            typeof webcamRef.current !== "undefined" &&
-            webcamRef.current !== null &&
-            webcamRef.current.video.readyState === 4
-        ) {
-            // Get Video Properties
-            const video = webcamRef.current.video;
-            const videoWidth = webcamRef.current.video.videoWidth;
-            const videoHeight = webcamRef.current.video.videoHeight;
+    useEffect(() => {
+        const statusOfHuman = sessionStorage.getItem('HumanIsVerified')
+        console.log("The Session is", statusOfHuman)
+        const url = "http://localhost:8000/success";
+                if (statusOfHuman === null) {
+                    const intervalId = setInterval(() => {  //assign interval to a variable to clear it.
+                        fetch(url)
+                            .then(response => response.json()).then(data => {
+                            console.log(data)
+                            if (data.Veracity === true) {
+                                sessionStorage.setItem('HumanIsVerified', true)
+                                // setIsOpen(false);
 
-            // Set video width
-            webcamRef.current.video.width = videoWidth;
-            webcamRef.current.video.height = videoHeight;
-
-            // Set canvas width
-            canvasRef.current.width = videoWidth;
-            canvasRef.current.height = videoHeight;
-
-            // Make Detections
-            // OLD MODEL
-            //       const face = await net.estimateFaces(video);
-            // NEW MODEL
-            // const face = await net.estimateFaces({input: video});
-            // console.log(face);
-
-            const predictions = await model.estimateFaces(video, returnTensors);
-            console.log(predictions);
-
-            // Get canvas context
-            try {
-                const context = canvasRef.current.getContext("2d");
+                                clearInterval(intervalId);
+                            }
 
 
-                // requestAnimationFrame(()=>{drawMesh(face, ctx)});
-                if (predictions.length > 0) {
+                        })
+                            .catch(function (error) {
+                                console.log(error)
 
-                    setState(true)
-                    /*
-                    `predictions` is an array of objects describing each detected face, for example:
-
-                    [
-                      {
-                        topLeft: [232.28, 145.26],
-                        bottomRight: [449.75, 308.36],
-                        probability: [0.998],
-                        landmarks: [
-                          [295.13, 177.64], // right eye
-                          [382.32, 175.56], // left eye
-                          [341.18, 205.03], // nose
-                          [345.12, 250.61], // mouth
-                          [252.76, 211.37], // right ear
-                          [431.20, 204.93] // left ear
-                        ]
-                      }
-                    ]
-                    */
-
-                    for (let i = 0; i < predictions.length; i++) {
-                        const start = predictions[i].topLeft;
-                        const end = predictions[i].bottomRight;
-                        var probability = predictions[i].probability;
-                        const size = [end[0] - start[0], end[1] - start[1]];
-                        // Render a rectangle over each detected face.
-                        context.beginPath();
-                        context.strokeStyle = "green";
-                        context.lineWidth = "4";
-                        context.rect(start[0], start[1], size[0], size[1]);
-                        context.stroke();
-                        var prob = (probability[0] * 100).toPrecision(5).toString();
-                        var text = prob + "%";
-                        context.fillStyle = "red";
-                        context.font = "13pt sans-serif";
-                        context.fillText(text, start[0] + 5, start[1] + 20);
-                    }
-                    sessionStorage.setItem('HumanIsVerified', true)
-                    setIsOpen(false);
+                            })
+                            .catch(function (error) {
+                                console.log(error)
+                            })
+                    }, 5000)
                 }
-            } catch (err) {
-                console.log(err)
-            }
 
-        }
+                //This is important
+
+            },
+            [url, useState]
+        )
+
+
+        return (
+            <>
+                <Sections/>
+                <Modal
+                    isOpen={modalIsOpen && !statusOfHuman}
+                    style={customStyles}
+                    contentLabel="Example Modal"
+                >
+
+                    {/*<button onClick={closeModal}>close</button>*/}
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            fontFamily: "'Montserrat', sans-serif",
+                            fontSize: "25px",
+                            color: Colors.WHITE_ISH,
+                            flexDirection: "row"
+                        }}
+                    >
+                        We Need To See If Your A Human
+
+                    </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            fontFamily: "'Montserrat', sans-serif",
+                            fontSize: "25px",
+                            color: Colors.WHITE_ISH,
+                            flexDirection: "row",
+                            marginTop: "20px"
+                        }}
+                    >
+                        <img style={{
+                            borderRadius: "15px"
+                        }} src={'/stream'} className="App-logo" alt="logo"/>
+
+                        <div
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                fontFamily: "'Montserrat', sans-serif",
+                                fontSize: "25px",
+                                color: Colors.WHITE_ISH,
+                                flexDirection: "row",
+                                marginTop: "30px"
+                            }}
+                        >
+                            {isVerifyLoading ? <CircularProgress style={{
+                                color: Colors.WHITE_ISH,
+                            }}/> : <></>
+
+                            }
+
+                        </div>
+                    </div>
+
+                </Modal>
+            </>
+        );
     }
 
 
-    useEffect(() => {
-            if (state === false) {
-                const intervalId = setInterval(() => {  //assign interval to a variable to clear it.
-                    runFacemesh();
-
-
-                        clearInterval(intervalId);
-
-                }, 5000)
-            }
-
-
-            //This is important
-
-        },
-        [url, useState]
-    )
-
-
-    return (
-        <>
-            <Sections/>
-            <Modal
-                isOpen={modalIsOpen && !statusOfHuman}
-                style={customStyles}
-                contentLabel="Example Modal"
-            >
-
-                {/*<button onClick={closeModal}>close</button>*/}
-                <div
-                    style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        fontFamily: "'Montserrat', sans-serif",
-                        fontSize: "25px",
-                        color: Colors.WHITE_ISH,
-                        flexDirection: "row"
-                    }}
-                >
-                    We Need To See If Your A Human
-
-                </div>
-                <div
-                    style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        fontFamily: "'Montserrat', sans-serif",
-                        fontSize: "25px",
-                        color: Colors.WHITE_ISH,
-                        flexDirection: "row",
-                        marginTop: "20px"
-                    }}
-                >
-                    {/*<img style={{*/}
-                    {/*    borderRadius: "15px"*/}
-                    {/*}} src={'/stream'} className="App-logo" alt="logo"/>*/}
-                    <Webcam
-                        ref={webcamRef}
-                        style={{
-                            position: "absolute",
-                            marginLeft: "auto",
-                            marginRight: "auto",
-                            overflowY: "hidden",
-                            textAlign: "center",
-                            zIndex: 9,
-                            width: 400,
-                            height: 400,
-                        }}
-                    />
-                    <canvas
-                        ref={canvasRef}
-                        style={{
-                            position: "absolute",
-                            marginLeft: "auto",
-                            marginRight: "auto",
-                            left: 0,
-                            right: 0,
-                            overflowY: "hidden",
-                            textAlign: "center",
-                            zIndex: 9,
-                            width: 400,
-                            height: 400,
-                        }}
-                    />
-                </div>
-                <div
-                    style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        fontFamily: "'Montserrat', sans-serif",
-                        fontSize: "25px",
-                        color: Colors.WHITE_ISH,
-                        flexDirection: "row",
-                        marginTop: "30px"
-                    }}
-                >
-                    {isVerifyLoading ? <CircularProgress style={{
-                        color: Colors.WHITE_ISH,
-                    }}/> : <></>
-
-                    }
-
-                </div>
-
-
-            </Modal>
-        </>
-    );
-};
-
-
-export default Homepage;
+    export default Homepage
